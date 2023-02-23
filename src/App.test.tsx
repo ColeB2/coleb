b09ae116419150b09d9e27961a194925a616d625
Cole <cebCole@gmail.com>
2023-02-23 13:27:23 -0600

    app test outline

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,10 +3,14 @@ import { render, screen } from '@testing-library/react';
 
 import App from './App';
 
+import { PROJECT_DATA } from './tests/testData';
+
 describe('App', () => {
-    it('Renders App component', () => {
+    it('Renders App component', async () => {
         // Arrange
-        render(<App />);
+        // const { getByText, getByPlaceholderText, queryByText } = render(<App />);
+        render(<App />)
+        screen.getByRole('');
         // Act
         // Expect
         // screen.debug();
@@ -16,6 +20,7 @@ describe('App', () => {
         //     screen.getByRole('generic', {
         //         level: 1,
         //     })
-        // ).toHaveTextContent('<divclass="boxscore-containers"/>');
+        // ).toHaveTextContent('<divclass="boxscore-containers"/>')
+        // console.log(getByText)
     });
 });
