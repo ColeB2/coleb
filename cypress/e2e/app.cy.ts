6737509c986bd7cebe2c17d21810d465cdcbb67c
Cole <cebCole@gmail.com>
2023-03-16 14:42:32 -0500

    more tests, expand tests later

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -21,11 +21,31 @@ describe('The Main Page', () => {
         cy.get('[alt="Project Name"]').first().click()
         cy.get('[alt="X icon to close"]').click()
     })
+    it('sucessfully clicks a project, and selects a featured project', () => {
+        cy.visit('/')
+        cy.contains('Featured Projects')
+        cy.get('[alt="Project Name"]').first().click()
+        // cy.get('[alt="Project Name2"]').parent().click()
+        cy.get('*[class^="project--related--item"]').last().click()
+    })
     it('sucessfully clicks a project, and closes project by clicking off', () => {
         cy.visit('/')
         cy.contains('Featured Projects')
         cy.get('[alt="Project Name"]').first().click()
-        cy.get('h5').contains('Featured Projects').click(5,300)
+        cy.window().then((win) => {
+            const posX = 50;
+            const posY = 220;
+            const options = {
+                button: 0,
+                force: true,
+                mutplie: false,
+                timeout: 4000,
+            };
+
+        const element = win.document.elementFromPoint(posX, posY) as HTMLElement;
+        cy.wrap(element).trigger('click', options);
+        })
+
     })
 
 })
